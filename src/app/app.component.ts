62dc7ef06d3436da13303bc6ba0ff131c3b7b23b
= <emforce@live.com>
2016-12-26 12:13:14 +0000

    added everything

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { WebsocketService } from './websocket.service'
+import { ChatService } from './chat.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ WebsocketService, ChatService ]
 })
 export class AppComponent {
-  title = 'app works!';
+
+	constructor(private chatService: ChatService) {
+		chatService.messages.subscribe(msg => {			
+      console.log("Response from websocket: " + msg);
+		});
+	}
+
+  private message = {
+		author: 'tutorialedge',
+		message: 'this is a test message'
+	}
+
+  sendMsg() {
+		console.log('new message from client to websocket: ', this.message);
+		this.chatService.messages.next(this.message);
+		this.message.message = '';
+	}
+
 }
---
+++ src/app/chat.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs/Rx';
+import { WebsocketService } from './websocket.service';
+
+const CHAT_URL = 'ws://echo.websocket.org/';
+
+export interface Message {
+	author: string,
+	message: string
+}
+
+@Injectable()
+export class ChatService {
+	public messages: Subject<Message>;
+
+	constructor(wsService: WebsocketService) {
+		this.messages = <Subject<Message>>wsService
+			.connect(CHAT_URL)
+			.map((response: MessageEvent): Message => {
+				let data = JSON.parse(response.data);
+				return {
+					author: data.author,
+					message: data.message
+				}
+			});
+	}
+}
